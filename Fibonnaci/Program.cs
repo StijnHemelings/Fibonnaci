1e5dba923520c9c7ed7c0eb6dc399831d852b1f4
Stijn Hemelings <csharpstijn@hotmail.com>
2020-06-14 00:00:39 +0200

    Add project files.

---
+++ Fibonnaci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Fibonnaci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int eerste = 0;
+            int tweede = 1;
+            int derde;
+
+            int aantal = 20;
+
+            Console.Write($"{eerste},{tweede}");
+
+            for (int i = 0; i < aantal; i++)
+            {
+                Console.Write(",");
+                derde = eerste + tweede;
+                Console.Write(derde);
+
+                eerste = tweede;
+                tweede = derde;
+            }
+            Console.ReadLine();
+        }
+    }
+}
